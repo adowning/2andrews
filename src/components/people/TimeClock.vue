e03f453ed3bf3fea90da69e2dbba9c3673d63cde
adowning <ashdowning@gmail.com>
2018-04-23 18:50:35 -0500

    asdf

--- src/components/people/TimeClock.vue
+++ src/components/people/TimeClock.vue
@@ -2,73 +2,61 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
-      <v-flex xs6>
-        <!-- <v-card dark color="primary"> -->
+      <!-- <v-flex xs6>
+        <v-card dark color="primary"> 
+        <v-card>
+          <v-card-text class="px-0">{{date}}</v-card-text>
+        </v-card> 
+      </v-flex> -->
+       <v-flex x6>
         <v-card>
           <v-card-text class="px-0">{{date}}</v-card-text>
         </v-card>
       </v-flex>
-       <v-flex xs6>
-        <v-card>
-          <v-card-text class="px-0">Time Sheets</v-card-text>
-        </v-card>
-      </v-flex>
+               
     </v-layout>
     <v-layout row wrap>
       <v-flex xs6>
-        <v-card>
-          <v-card-text class="px-0">
-            <v-stepper  vertical>
-              <v-stepper-step  v-show="!clockedIn" step="1">
-               <v-btn color="primary" @click.native="clockIn()">Clock In</v-btn>
-              {{ startTime }}
-                  <v-list-tile-content >
-                <v-list-tile-title>{{ startTime }}</v-list-tile-title>
+        <v-layout row>
+    <v-flex xs8>
+      <v-card>
+        <v-toolbar color="cyan">
+          <v-toolbar-title>{{date}}</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <!-- <v-btn icon>
+            <v-icon>search</v-icon> 
+          </v-btn>-->
+        </v-toolbar>
+        <v-list two-line>
+              <v-list-tile-content v-if="!clockStatus">
+           <v-btn block color="primary" dark @click.native="clockIn">Clock In</v-btn>
               </v-list-tile-content>
-              </v-stepper-step>
-              <!-- <v-stepper-content step="1">
-              </v-stepper-content> -->
-               <v-stepper v-model="e1" vertical>
-      <!-- <v-stepper-header> -->
-        <template v-for="n in steps">
-          <v-stepper-step  v-show="clockedIn"
-            :key="`${n}-step`"
-            :step="n"
-            :complete="e1 > n"
-          >
-             <v-flex xs12 sm6  v-show="clockedIn">
-        <v-text-field box label="Notes" v-model="first"></v-text-field>
+             <v-list-tile-content v-if="clockStatus">
+                  {{startTime}} 
+              </v-list-tile-content>
+           <!-- <template v-for="(item, index) in items">
+            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+            <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+            <v-list-tile avatar v-else :key="item.title" @click="">
+              <v-list-tile-avatar>
+                <img :src="item.avatar">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>  -->
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
       </v-flex>
-
-          </v-stepper-step>
-          <!-- <v-divider v-if="n !== steps" :key="n"></v-divider> -->
-        </template>
-      <!-- </v-stepper-header> -->
-      <!-- <v-stepper-items> -->
-        <!-- <v-stepper-content
-          :step="n"
-          v-for="n in steps"
-          :key="`${n}-content`"
-        >
-         
-        </v-stepper-content> -->
-      <!-- </v-stepper-items> -->
-            </v-stepper>
-              <v-stepper-step  v-show="clockedIn" step="6">
-               <v-btn color="warning" @click.native="clockOut()">Clock Out</v-btn>                
-              </v-stepper-step>
-              <!-- <v-stepper-content step="2">
-               
-              </v-stepper-content> -->
-            </v-stepper>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs6>
+      <!-- <v-flex xs6>
         <v-card dark color="secondary">
           <v-card-text class="px-0">Time Sheets</v-card-text>
         </v-card>
-      </v-flex>
+      </v-flex> -->
     </v-layout>
   </v-container>
 </template>
@@ -84,17 +72,24 @@ import moment from 'moment'
 //   ClientId: '257pbc9k89flmkiq0cq5a7epl9'
 // }
 export default {
-  data: () => ({
-    date: moment().format('MMMM Do YYYY - h:mm a'),
-     e13: 2,
-     e1: 1,
-     steps: 1,
-     clockedIn: false,
-     first: '',
-     startTime: '',
-     endTime: ''
-  }),
+  data () {
+      return {
+        clockStatus: false,
+        date: moment().format('LLLL'),
+        items: [
+          { header: 'Today' },
+          { avatar: '/static/doc-images/lists/1.jpg', title: 'Brunch this weekend?', subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/lists/2.jpg', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend." },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/lists/3.jpg', title: 'Oui oui', subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?" }
+        ]
+      }
+    },
    watch: {
+    //  clockedIn (val) {
+    //       console.log(val)
+    //  },
       steps (val) {
         if (this.e1 > val) {
           this.e1 = val
@@ -104,8 +99,9 @@ export default {
 
     methods: {
       clockIn(){
+        console.log('asdf')
           this.startTime =  moment().format('h:mm a'),
-          this.clockedIn = true
+          this.clockStatus = true
       },
         clockOut(){
           this.endTime = new moment()
