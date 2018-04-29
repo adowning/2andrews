caa355b9c58a0e77bf07f62a3a14d6cbb3dc8c3f
adowning <ashdowning@gmail.com>
2018-04-28 19:56:43 -0500

    h=desk

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo $DATE=`date '+%Y-%m-%d %H:%M:%S'` > .gitupdate.txt
 yarn lint
- netlify-lambda build lambda-src
- git add .
-#  git commit -m `date '+%Y-%m-%d %H:%M:%S'`
-git commit -m 'adsfasdfdsf'
- git push origin appsync
+netlify-lambda build lambda-src
+git add .
+git commit -m h=$(hostname -f)
+git push origin appsync --force
+rm .gitupdate.txt
