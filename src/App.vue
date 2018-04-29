58446d6f92329be7dccb4cb3e92dd78a26bf92ef
adowning <ashdowning@gmail.com>
2018-04-29 02:29:34 -0500

    h=desk

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,6 @@
   </div>
 </template>
 <script>
-// Check that service workers are registered
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-  })
-}
-</script>
-<script>
-// Check that service workers are registered
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-  })
-}
 </script>
 
 <style>
--- static/sw.js
+++
@@ -1,8 +0,0 @@
-/*eslint-disable */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
-
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
