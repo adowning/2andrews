b744e51a5f6f5807d9102f1a001847d7408cf50c
adowning <ashdowning@gmail.com>
2018-04-29 01:45:20 -0500

    h=desk

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,22 @@
     </v-app>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'App',
-  created() {
-    this.$emit('test', 'hi')
-  },
+// Check that service workers are registered
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
+</script>
+<script>
+// Check that service workers are registered
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
 }
 </script>
 
